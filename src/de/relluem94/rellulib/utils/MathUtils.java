dbd3d8adc533295f58febd05de805e1b224a2025
Relluem94 <github@relluem94.de>
2020-12-31 16:47:48 +0100

    fixing warnings

--- src/de/relluem94/rellulib/utils/MathUtils.java
+++ src/de/relluem94/rellulib/utils/MathUtils.java
@@ -29,11 +29,7 @@ public class MathUtils {
      * @return boolean
      */
     public static boolean isOdd(int number) {
-        if (number % 2 == 0) {
-            return false;
-        } else {
-            return true;
-        }
+        return number % 2 != 0;
     }
 
     /**
