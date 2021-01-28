79674580a0cbb466304369c7e0f9666fbf1cec41
Relluem94 <github@relluem94.de>
2021-01-29 00:21:00 +0100

    format

--- src/main/java/de/relluem94/rellulib/sounds/NoteLength.java
+++ src/main/java/de/relluem94/rellulib/sounds/NoteLength.java
@@ -20,33 +20,35 @@ public enum NoteLength {
     public static int getLength(NoteLength s) {
         int length = 0;
 
-        if (null != s) switch (s) {
-            case None:
-                length = 0;
-                break;
-            case Quarter:
-                length = 286;
-                break;
-            case Dotted_Quarter:
-                length = 429;
-                break;
-            case Whole:
-                length = 1144;
-                break;
-            case Dotted_Half:
-                length = 858;
-                break;
-            case Half:
-                length = 572;
-                break;
-            case Eighth:
-                length = 143;
-                break;
-            case Sixteenth:
-                length = 72;
-                break;
-            default:
-                break;
+        if (null != s) {
+            switch (s) {
+                case None:
+                    length = 0;
+                    break;
+                case Quarter:
+                    length = 286;
+                    break;
+                case Dotted_Quarter:
+                    length = 429;
+                    break;
+                case Whole:
+                    length = 1144;
+                    break;
+                case Dotted_Half:
+                    length = 858;
+                    break;
+                case Half:
+                    length = 572;
+                    break;
+                case Eighth:
+                    length = 143;
+                    break;
+                case Sixteenth:
+                    length = 72;
+                    break;
+                default:
+                    break;
+            }
         }
 
         return length;
