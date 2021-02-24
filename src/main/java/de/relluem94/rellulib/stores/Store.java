5bd8542cb1c1e57b3089d059ffe6cae12f0945e7
Relluem94 <github@relluem94.de>
2021-02-24 23:00:08 +0100

    added hashCode and equals

--- src/main/java/de/relluem94/rellulib/stores/Store.java
+++ src/main/java/de/relluem94/rellulib/stores/Store.java
@@ -1,5 +1,7 @@
 package de.relluem94.rellulib.stores;
 
+import java.util.Objects;
+
 public class Store implements IStore {
 
     private Object value;
@@ -25,5 +27,25 @@ public class Store implements IStore {
     @Override
     public String toString() {
         return "Value: " + this.value;
+    }
+    
+    @Override
+    public int hashCode(){
+        return (int) 69 * value.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Store other = (Store) obj;
+        return Objects.equals(this.value, other.value);
     }
 }
