80f6e15074365f50f9cf4e1ef5cfcff612ec0645
Relluem94 <github@relluem94.de>
2023-07-26 01:40:31 +0200

    linted

--- src/main/java/de/relluem94/rellulib/vector/Vector5i.java
+++ src/main/java/de/relluem94/rellulib/vector/Vector5i.java
@@ -4,15 +4,11 @@ import java.text.DecimalFormat;
 
 public class Vector5i implements IVector {
 
-    public int x;
-
-    public int y;
-
-    public int z;
-
-    public int w;
-
-    public int v;
+    private int x;
+    private int y;
+    private int z;
+    private int w;
+    private int v;
 
     public Vector5i(int x, int y, int z, int w, int v) {
         this.x = x;
@@ -87,4 +83,43 @@ public class Vector5i implements IVector {
         return "" + x + "," + y + "," + z + "," + w + "," + v;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public void setV(int v) {
+        this.v = v;
+    }
 }
