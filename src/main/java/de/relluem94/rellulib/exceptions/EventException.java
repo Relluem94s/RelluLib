b93404b97b970590e02b17d2af62b4874019cf69
Relluem94 <github@relluem94.de>
2025-04-11 21:18:08 +0200

    RL-4 Exception CleanUp
    Signed-off-by: Relluem94 <github@relluem94.de>

--- src/main/java/de/relluem94/rellulib/exceptions/EventException.java
+++ src/main/java/de/relluem94/rellulib/exceptions/EventException.java
@@ -1,7 +1,10 @@
 package de.relluem94.rellulib.exceptions;
 
+import java.io.Serial;
+
 public class EventException extends Exception {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public EventException(String message) {
--- src/main/java/de/relluem94/rellulib/exceptions/RelluLibException.java
+++ src/main/java/de/relluem94/rellulib/exceptions/RelluLibException.java
@@ -1,7 +1,11 @@
 package de.relluem94.rellulib.exceptions;
 
+import java.io.Serial;
+
+@Deprecated
 public class RelluLibException extends Exception {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public RelluLibException(String message) {
