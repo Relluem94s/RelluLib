5df679a7873a6f78c270c047aa9595ba344f58a1
Relluem94 <github@relluem94.de>
2025-04-11 21:45:35 +0200

    RL-6 Store CleanUp
    Signed-off-by: Relluem94 <github@relluem94.de>

--- src/test/java/de/relluem94/rellulib/stores/DoubleStoreTest.java
+++ src/test/java/de/relluem94/rellulib/stores/DoubleStoreTest.java
@@ -1,11 +1,9 @@
 package de.relluem94.rellulib.stores;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class DoubleStoreTest {
 
