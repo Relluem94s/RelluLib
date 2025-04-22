5871a3ce4a110432d451fbc3151b3ee1fa56130a
Relluem94 <github@relluem94.de>
2025-04-22 18:24:04 +0200

    RL-31 DoubleStore Code Coverage
    Signed-off-by: Relluem94 <github@relluem94.de>

--- src/main/java/de/relluem94/rellulib/stores/DoubleStore.java
+++ src/main/java/de/relluem94/rellulib/stores/DoubleStore.java
@@ -33,7 +33,7 @@ public class DoubleStore<T, T2> extends Store<T> implements IDoubleStore<T, T2> 
     
     @Override
     public int hashCode(){
-        return 42 * getValue().hashCode() * Objects.hashCode(secondValue);
+        return 42 *  Objects.hashCode(getValue()) * Objects.hashCode(secondValue);
     }
 
     @Override
