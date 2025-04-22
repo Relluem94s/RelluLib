cdd4a1e6cb7b05440d166ec74df35280e6d383e8
Relluem94 <github@relluem94.de>
2025-04-22 18:29:18 +0200

    RL-58 EventException Code Coverage
    Signed-off-by: Relluem94 <github@relluem94.de>

---
+++ src/test/java/de/relluem94/rellulib/exceptions/EventExceptionTest.java
@@ -0,0 +1,25 @@
+package de.relluem94.rellulib.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventExceptionTest {
+
+    @Test
+    public void testEventExceptionConstructorWithMessage() {
+        String errorMessage = "Test event exception message";
+        EventException exception = new EventException(errorMessage);
+
+        assertNotNull(exception, "Exception should not be null");
+        assertEquals(errorMessage, exception.getMessage(), "Exception message should match the provided message");
+    }
+
+    @Test
+    public void testEventExceptionIsInstanceOfException() {
+        EventException exception = new EventException("Test message");
+        assertInstanceOf(Exception.class, exception, "EventException should be an instance of Exception");
+    }
+}
