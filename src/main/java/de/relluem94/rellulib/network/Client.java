a1b2f6d5e869273fccfa8c9d8228abb025eb5820
Relluem94 <github@relluem94.de>
2025-05-02 16:00:47 +0200

    RL-23 Client Rework for Test Coverage
    Signed-off-by: Relluem94 <github@relluem94.de>

--- src/main/java/de/relluem94/rellulib/network/Client.java
+++ src/main/java/de/relluem94/rellulib/network/Client.java
@@ -1,20 +1,35 @@
 package de.relluem94.rellulib.network;
 
+import de.relluem94.rellulib.utils.LogUtils;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class Client {
 
-    private final Socket socket = new Socket();
+    private final Socket socket;
+
+    public Client(){
+        socket = new Socket();
+    }
 
     public boolean connect(String host, int port) throws IOException {
         int timeout = 60;
+        if (socket.isClosed()) {
+            LogUtils.error("Client socket is already closed before connect");
+            throw new IllegalStateException("Socket is closed");
+        }
+        LogUtils.info("Client attempting to connect to " + host + ":" + port);
         socket.connect(new InetSocketAddress(host, port), timeout);
+        LogUtils.info("Client connection status: " + socket.isConnected());
         return socket.isConnected();
     }
 
     public void close() throws IOException {
-        socket.close();
+        if (!socket.isClosed()) {
+            LogUtils.info("Closing client socket");
+            socket.close();
+        }
     }
 }
