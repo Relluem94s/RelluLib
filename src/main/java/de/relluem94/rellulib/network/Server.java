aa53d473bef9f99fe690a22f60401d38799e24b8
Relluem94 <github@relluem94.de>
2025-05-02 16:01:03 +0200

    RL-26 Server Rework for Test Coverage
    Signed-off-by: Relluem94 <github@relluem94.de>

--- src/main/java/de/relluem94/rellulib/network/Server.java
+++ src/main/java/de/relluem94/rellulib/network/Server.java
@@ -1,5 +1,7 @@
 package de.relluem94.rellulib.network;
 
+import de.relluem94.rellulib.utils.LogUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,13 +10,57 @@ import java.net.Socket;
 
 public class Server {
 
-    public void start(int port) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(port);
-        Socket socket = serverSocket.accept();
+    private final ServerSocket serverSocket;
+    private Socket socket;
+    BufferedReader bufferedReader;
+    private volatile boolean isRunning = true;
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        
-        bufferedReader.close();
+    public Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
     }
 
+    public void start() throws IOException {
+        while (isRunning && !serverSocket.isClosed()) {
+            try {
+                socket = serverSocket.accept();
+                bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                bufferedReader.close();
+
+                if (socket != null && !socket.isClosed()) {
+                    socket.close();
+                }
+            }
+            catch (IOException e) {
+                if (!isRunning || serverSocket.isClosed()) {
+                    break;
+                }
+                throw e;
+            }
+
+        }
+    }
+
+    public void stop() throws IOException {
+        isRunning = false;
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+
+            if (!serverSocket.isClosed()) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            LogUtils.error("Error closing server socket: " + e.getMessage());
+        }
+    }
+
+
+    public int getPort() {
+        return serverSocket.getLocalPort();
+    }
 }
